8c16c51098aaee25c00baf457684d35bb725e155
r-andomguy <bruno200coelho@hotmail.com>
2025-02-07 00:50:03 -0300

    feat: adiciona rota principal e de autores.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+    return view('home');
+})->name('home');
 
+Route::resource('authors', AuthorController::class);
 require __DIR__ . '/auth.php';
