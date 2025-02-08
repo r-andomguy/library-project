5be22c0b768d7729c6040c0fca7eb14a422e2e86
r-andomguy <bruno200coelho@hotmail.com>
2025-02-08 02:03:45 -0300

    feat: implementa controladores de autenticação, adiciona rotas de login, logout e authors para API.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', [AuthorController::class, 'auth']);
+Route::post('logout', [AuthorController::class, 'logout'])->middleware('auth:sanctum');
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('authors', AuthorController::class);
 });
