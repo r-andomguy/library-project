7ce21fed736f9376898f0b8d66ebb6774af667dc
r-andomguy <bruno200coelho@hotmail.com>
2025-02-08 03:06:36 -0300

    fix: atualiza função de login.

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,23 +5,20 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\AuthRequest;
 use App\Models\User;
-use http\Env\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller {
 
-    public function auth (AuthRequest $request) {
+    public function login (AuthRequest $request) {
         $user = User::where('email', $request->email)->first();
 
-        if(!$user || !Hash::check($request->password, $user->password)){
-            throw ValidationException::withMessages([
-                'email' => ['As credenciais do usuário não estão corretas.']
-            ]);
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'Credenciais inválidas'], 401);
         }
 
         $user->tokens()->delete();
-        $token = $user->createToken($request->email)->plainTextToken;
+        $token = $user->createToken('api_token')->plainTextToken;
 
         return response()->json([
             'token' => $token
@@ -30,10 +27,7 @@ class AuthController extends Controller {
 
     public function logout (Request $request) {
         $request->user()->tokens()->delete();
-
-        return response()->json([
-            'message' => 'success'
-        ]);
+        return response()->json(['message' => 'Logout realizado com sucesso']);
     }
 
 }
