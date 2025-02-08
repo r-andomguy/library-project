f14e195acb1603e028357f911de6b668a11c213a
r-andomguy <bruno200coelho@hotmail.com>
2025-02-08 02:25:50 -0300

    fix: corrige controlador de autenticação nas rotas de api, utilização de FormRequest.

--- app/Http/Requests/Api/AuthRequest.php
+++ app/Http/Requests/Api/AuthRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
+use Illuminate\Foundation\Http\FormRequest;
+
 class AuthRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\AuthorController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [AuthorController::class, 'auth']);
-Route::post('logout', [AuthorController::class, 'logout'])->middleware('auth:sanctum');
-
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('login', [AuthController::class, 'auth']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('authors', AuthorController::class);
