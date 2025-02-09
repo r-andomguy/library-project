9ba6f81072b1dfb15831d2b8b2ed8f75506234c7
r-andomguy <bruno200coelho@hotmail.com>
2025-02-09 20:44:57 -0300

    feat: adiciona type para os cadastro de usuário no seed.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\UserType;
 use Illuminate\Database\Seeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,18 +18,21 @@ class DatabaseSeeder extends Seeder {
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'type' => UserType::DEFAULT
         ]);
 
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
-            'password' => bcrypt('123')
+            'password' => bcrypt('123'),
+            'type' => UserType::ADMIN
         ]);
 
         User::factory()->create([
             'name' => 'Bruno',
             'email' => 'bruno@example.com',
-            'password' => bcrypt('123')
+            'password' => bcrypt('123'),
+            'type' => UserType::DEFAULT
         ]);
 
         $this->call([
